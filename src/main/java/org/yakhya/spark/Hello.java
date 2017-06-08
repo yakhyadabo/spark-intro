43d2836594dcaee6a4a2e958ba60cd11afaf6925
Yakhya Dabo <yakhyadabo@gmail.com>
2017-06-08 15:50:04 +0100

    Initial commit

---
+++ src/main/java/org/yakhya/spark/Hello.java
@@ -0,0 +1,13 @@
+package org.yakhya.spark;
+
+import static spark.Spark.get;
+
+public class Hello {
+
+  public static void main(String[] args) {
+    get("/", (req, res) -> {
+      return "hello from sparkjava.com";
+    });
+  }
+
+}
